fb032b32f96e099101713606761c59b99ff84641
jalajthanaki <jalajthanaki@gmail.com>
2020-08-17 00:57:28 +0530

    Added openapi extension

--- src/main/java/org/test/Example.java
+++ src/main/java/org/test/Example.java
@@ -5,12 +5,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
-public class ExampleResource {
+@Path("/explorer")
+public class Example {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello world";
+        return "world";
     }
 }
